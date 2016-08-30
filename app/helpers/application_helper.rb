db6181d81043e5ad0fcf6baf0c4043086aee7955
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-08-30 16:47:39 -0400

    Implement title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # Returns full title on per-page basis.
+  def full_title(page_title = nil)
+    base_title = "Sample App"
+    page_title ? "#{ page_title } | #{ base_title }" : base_title
+  end
+
 end
