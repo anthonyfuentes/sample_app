b4e0c454ff1a94e719ae59392ccc6f1afcbdf48d
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-08-30 17:05:09 -0400

    Debug full title method Also updated static_pages_controller tests"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
   # Returns full title on per-page basis.
-  def full_title(page_title = nil)
+  def full_title(page_title = "")
     base_title = "Sample App"
-    page_title ? "#{ page_title } | #{ base_title }" : base_title
+    page_title.empty? ? base_title : "#{ page_title } | #{ base_title }"
   end
 
 end
