1795e9fe34a8aed25ebf47ca85e54acc538d95b8
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-09-12 12:33:52 -0400

    Complete user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save ? welcome : render('new')
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    def welcome
+      redirect_to(@user)
+      flash[:success] = "Welcome to Sample App"
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get   '/contact',   to: 'static_pages#contact'
   get   '/error_404', to: 'static_pages#error_404'
   get   '/signup',    to: 'users#new'
+  post  '/signup',    to: 'users#create'
+  resources :users
 end
 
